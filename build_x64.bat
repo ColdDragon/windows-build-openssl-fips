0021530af83a489b2bbcf248d35f7b18b94b80fb
ColdDragon <33009904+ColdDragon@users.noreply.github.com>
2018-01-02 17:28:37 +0900

    Add files via upload

--- build_x64.bat
+++ build_x64.bat
@@ -1,5 +1,7 @@
 cd openssl-1.0.2n
 rmdir /S/Q tmp32dll
 perl Configure VC-WIN64A --openssldir=E:\openssl\out_x64 threads no-idea no-mdc2 no-rc5 no-asm
+perl -pi'.bak' -e 's/-WX//g' Makefile
 call ms\do_win64a.bat
 nmake -f ms\ntdll.mak install
+cd ..
--- build_x86.bat
+++ build_x86.bat
@@ -1,5 +1,7 @@
 cd openssl-1.0.2n
 rmdir /S/Q tmp32dll
 perl Configure VC-WIN32 --openssldir=E:\openssl\out_x86 threads no-idea no-mdc2 no-rc5
+perl -pi'.bak' -e 's/-WX//g' Makefile
 call ms\do_nasm.bat
 nmake -f ms\ntdll.mak install
+cd ..
